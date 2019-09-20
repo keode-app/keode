aa31754180974dbdee391688d986870ffc465cee
Jan Müller <janmueller3698@gmail.com>
2019-09-20 16:32:07 +0200

    Added minimal anko setup

--- app/src/main/java/com/github/keode/MainActivity.kt
+++ app/src/main/java/com/github/keode/MainActivity.kt
@@ -1,39 +1,37 @@
 package com.github.keode
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        MainActivityUI().setContentView(this)
     }
+}
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+class MainActivityUI : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+        verticalLayout {
+            toolbar {
+                padding = dip(3)
+                title = "Keode"
+                backgroundColor = Color.parseColor("#008577")
+                relativeLayout {
+                    button("Settings") {
+                        onClick {
+                            alert("Under construction").show()
+                        }
+                    }.lparams {
+                        alignParentEnd()
+                    }
+                }
+            }
         }
     }
 }
