fba36258219402719fedcec5efd559f4969202d2
Jan Müller <janmueller3698@gmail.com>
2019-09-21 17:13:33 +0200

    Added remaining Activity stubs

---
+++ app/src/main/java/com/github/keode/CategorizationActivity.kt
@@ -0,0 +1,22 @@
+package com.github.keode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.*
+
+class CategorizationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        CategorizationActivityUI().setContentView(this)
+    }
+}
+
+class CategorizationActivityUI : AnkoComponent<CategorizationActivity> {
+    override fun createView(ui: AnkoContext<CategorizationActivity>) = with(ui) {
+        relativeLayout {
+            textView("Under construction").lparams { centerInParent() }
+        }
+    }
+}
