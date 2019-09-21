66cc502392fb7f4bec79916fbc3f94bfbcfcba32
Jan Müller <janmueller3698@gmail.com>
2019-09-21 18:12:12 +0200

    Fixed broken auto generated test

--- app/src/androidTest/java/com/github/keode/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/keode/ExampleInstrumentedTest.kt
@@ -1,11 +1,8 @@
 package com.github.keode
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
@@ -13,8 +10,8 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
